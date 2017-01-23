7dfa85789bff20aa7bb2d6423a7f58efcecd9bdb
angelsilva94 <angelsilva_94@hotmail.com>
2017-01-23 08:36:56 -0600

    3N+1 Problem Solved

---
+++ 3n+1/3n+1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace _3n_1{
+    class Program{
+        static void Main(string[] args){
+            int numb;
+            Console.WriteLine("Give me a number:");
+            numb = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The number is: " + numb);
+            do {
+                if (numb != 1) {
+                    if (numb % 2 != 0) {
+                        numb = (numb * 3) + 1;
+                        //numb *= 3 + 1;
+                    } else {
+                        numb /= 2;
+                    }
+                    Console.WriteLine("The number is: " + numb);
+                }
+
+            } while (numb != 1);
+            Console.ReadKey();
+        }
+    }
+}
