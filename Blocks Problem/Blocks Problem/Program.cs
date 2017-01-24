0423f90e92835e2b51811ff8895373bec16e0356
angelsilva94 <angelsilva_94@hotmail.com>
2017-01-24 10:53:18 -0600

    Blocks Problem

--- Blocks Problem/Blocks Problem/Program.cs
+++ Blocks Problem/Blocks Problem/Program.cs
@@ -6,28 +6,13 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Blocks_Problem {
-
-
-
-
-    
     class Program {
-        static int a, b;
-        public static Stack<int>[] Blocks;
-        public static int[] pos;
         static void Main(string[] args) {
-            int numb,op;
-            
+            int op;
             Console.WriteLine("Welcome");
             Console.WriteLine("Give me the lenght of the block:");
-            numb = int.Parse(Console.ReadLine());
-            pos = new int[numb];
-            Blocks = new Stack<int>[numb];
-            for (int i = 0; i < numb; i++) {
-                Blocks[i] = new Stack<int>();
-                Blocks[i].Push(i);
-                pos[i] = i;
-            }
+            //numb = int.Parse(Console.ReadLine());
+            Block obj = new Block(int.Parse(Console.ReadLine()));
             do {
                 Console.WriteLine("What do you wanna do?");
                 Console.WriteLine("Move a onto b....press 1");
@@ -38,20 +23,20 @@ namespace Blocks_Problem {
                 op = int.Parse(Console.ReadLine());
                 switch (op) {
                     case 1:
-                        Menu();
-                        MoveOnto(a,b);
+                        obj.Ask();
+                        obj.MoveOnto();
                         break;
                     case 2:
-                        Menu();
-                        MoveOver(a,b);
+                        obj.Ask();
+                        obj.MoveOver();
                         break;
                     case 3:
-                        Menu();
-                        PileOnto(a,b);
+                        obj.Ask();
+                        obj.PileOnto();
                         break;
                     case 4:
-                        Menu();
-                        PileOver(a,b);
+                        obj.Ask();
+                        obj.PileOver();
                         break;
                     case 5:
                         break;
@@ -61,66 +46,10 @@ namespace Blocks_Problem {
                 }
             } while (op != 5);
 
-            for (int i = 0; i < Blocks.Length; i++) {
-                Console.WriteLine(Sol(i));
+            for (int i = 0; i < obj.length; i++) {
+                Console.WriteLine(obj.Sol(i));
             }
-                
-
             Console.ReadKey();
-
         }
-        public static void Menu() {
-            Console.WriteLine("Give me A and B:");
-            a = int.Parse(Console.ReadLine());
-            b = int.Parse(Console.ReadLine());
-        }
-        public static void MoveOnto(int a, int b) {
-            Clear(b);
-            Clear(a);
-            Blocks[pos[b]].Push(Blocks[pos[a]].Pop());
-            pos[a] = pos[b];
-
-        }
-        public static void Clear(int Block) {
-            while(Blocks[pos[Block]].Peek() != Block) {
-                Initial(Blocks[pos[Block]].Pop());
-            }
-        }
-        public static void Initial(int Block) {   
-            while(Blocks[Block].Count()>0) {
-                Initial(Blocks[Block].Pop());
-            }
-            Blocks[Block].Push(Block);
-            pos[Block] = Block;
-        }
-        public static void MoveOver(int a, int b) {
-            Clear(a);
-            Blocks[pos[b]].Push(Blocks[pos[a]].Pop());
-            pos[a] = pos[b];
-        }
-        public static void PileOnto(int a, int b) {
-            Clear(b);
-            PileOver(a, b);
-        }
-        public static void PileOver(int a, int b) {
-            Stack<int> Pile = new Stack<int>();
-            while (Blocks[pos[a]].Peek() != a) {
-                Pile.Push(Blocks[pos[a]].Pop());
-            }
-            Pile.Push(Blocks[pos[a]].Pop());
-            while (Pile.Count>0) {
-                int aux = Pile.Pop();
-                Blocks[pos[b]].Push(aux);
-                pos[aux] = pos[b];
-            }
-        }
-        public static String Sol(int index) {
-            String res = "";
-            while (Blocks[index].Count>0)
-                res = " " + Blocks[index].Pop() + res;
-                res = index + ":" + res;
-            return res;
-        }
-
     }
 }
