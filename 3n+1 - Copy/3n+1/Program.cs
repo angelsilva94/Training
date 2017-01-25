d4207e90582918b380cd21e4c9c2ac4df4297612
angelsilva94 <angelsilva_94@hotmail.com>
2017-01-25 12:09:11 -0600

    Final

--- 3n+1 - Copy/3n+1/Program.cs
+++ 3n+1 - Copy/3n+1/Program.cs
@@ -5,17 +5,16 @@ using System.Text;
 using System.Threading.Tasks;
 using threeN.Validator;
 using threeN.Process;
-namespace _3n_1 {
-    class Program {
-        static void Main(string[] args) {
+namespace _3n_1{
+    class Program{
+        static void Main(string[] args){
             Validator val = new Validator();
             val.Ask();
-            if (val.Val()) {
-                ;
+            if (val.Val()) {;
                 ProcessInput PI = new ProcessInput(val.i, val.j);
-                int res = PI.ProcessNumber();
-                Console.WriteLine(val.i + " " + val.j + " " + res);
-                foreach (var l in PI.result) {
+                int res= PI.ProcessNumber();
+                Console.WriteLine(val.i + " " + val.j +" "+ res);
+                foreach(var l in PI.result) {
                     Console.WriteLine(l);
 
                 }
