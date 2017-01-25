a08a5fd9f49c55b726d59c1da6495f79f78a3014
Otman Licona <otmanlicona@hotmail.com>
2017-01-25 09:37:45 -0600

    ProcessMessage

--- 3n+1/3n+1/ProcessInput.cs
+++ 3n+1/3n+1/ProcessInput.cs
@@ -8,12 +8,39 @@ namespace _3n_1
 {
     public class ProcessInput
     {
-        public List<int> Process(int i, int j)
+        public List<int> ProcessPair(int i, int j)
         {
             List<int> result = new List<int>();
 
+            result.Add(i);
+            result.Add(j);
+            result.Add(this.ProcessNumber(i) + this.ProcessNumber(j) + 2);
             return result;
 
+
+        }
+
+        private int ProcessNumber(int x)
+        {
+            int cont = 0;
+            do
+            {
+                if (x != 1)
+                {
+                    if (x % 2 != 0)
+                    {
+                        x = (x * 3) + 1;
+
+                    }
+                    else
+                    {
+                        x /= 2;
+                    }
+                    cont++;
+
+                }
+            } while (x != 1);
+            return cont;
         }
     }
 }
--- 3n+1/3n+1/Program.cs
+++ 3n+1/3n+1/Program.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 namespace _3n_1{
     class Program{
         static void Main(string[] args){
             int numb;
+            int cont = 0;
             Console.WriteLine("Give me a number:");
             numb = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("The number is: " + numb);
@@ -18,11 +20,12 @@ namespace _3n_1{
                     } else {
                         numb /= 2;
                     }
-                    //cont++;
-                    Console.WriteLine("The number is: " + numb);
+                    cont++;
+                    
                 }
 
             } while (numb != 1);
+            Console.WriteLine("The number is: " + cont);
             Console.WriteLine("Pres any key to exit");
             Console.ReadKey();
         }
