88305e6bae6f28fba951c5bca57c504c3b508708
angelsilva94 <angelsilva_94@hotmail.com>
2017-02-16 13:02:42 -0600

    blocks httppost json

--- WebApplication/WebApplication/paginas/scriptsBlocks.js
+++ WebApplication/WebApplication/paginas/scriptsBlocks.js
@@ -1,6 +1,6 @@
 var app = angular.module('blocks', []);
 
-app.controller('mainController', function ($scope, $window) {
+app.controller('mainController', function ($scope, $window, $http) {
 
   $scope.lenghtSubmit = function () {
     if ($scope.blockLength < 0 || $scope.blockLength > 25) {
@@ -8,56 +8,82 @@ app.controller('mainController', function ($scope, $window) {
       $scope.blockLength = "";
     } else {
       $scope.lenghtPanel = true;
+      $scope.panelAsk = true;
     }
 
   }
   $scope.moves = [];
+  $scope.userMoves = [];
+  var serverMove = [];
+  var serverA = [];
+  var serverB = [];
+
   $scope.addMove = function () {
-    $scope.moves.push($scope.addMe);
+    $scope.userMoves.push($scope.addA + " " + $scope.addMov + " " + $scope.addB);
+    $scope.tableMove = true;
+    serverA.push($scope.addA);
+    serverB.push($scope.addB);
+    serverMove.push($scope.addMov);
   }
   $scope.removeMove = function (x) {
-    $scope.moves.splice(x, 1);
+    $scope.userMoves.splice(x, 1);
+  }
+  $scope.sendServer = function () {
+    $scope.moves.push("end");
+    $scope.panelAsk = false;
+    $scope.panelIns = true;
+    $scope.panelResults = true;
+    /*
+    var objetoJson = {
+      "instructions": [{
+        "A": $scope.addA,
+        "B": $scope.addB,
+        "move": $scope.addMov
+      }, {
+        "A": $scope.addA,
+        "B": $scope.addB,
+        "move": $scope.addMov
+      }],
+      "length": $scope.blockLength,
+      "res": null
+    };*/
+    var objetoJson = [];
+    /*for (var i = 0; i < serverA.length; i++) {
+      objetoJson[i] = {
+        "instructions": [{
+          "A": serverA[i],
+          "B": serverB[i],
+          "move": serverMove[i]
+        }],
+        "length": $scope.blockLength,
+        "res": null
+      };
+    }*/
+    Json = {
+      "instructions": [{
+        "A": $scope.addA,
+        "B": $scope.addB,
+        "move": $scope.addMov
+      }, {
+        "A": $scope.addA,
+        "B": $scope.addB,
+        "move": $scope.addMov
+      }],
+      "length": $scope.blockLength,
+      "res": null
+    };
+
+    $http.post("http://localhost:56493/api/BlocksProblem", Json)
+      .then(function (data) {
+        $scope.result = data;
+      }, function (response) {
+        $scope.result = response;
+      });
+
+
+
   }
 
 
 
 });
-
-/*
-app.controller('mainController', function ($scope, $http) {
-
-  $scope.numbSubmit = function () {
-
-    $http.get("http://localhost:56493/api/ThreeN", {
-        params: {
-          x: $scope.numberx,
-          y: $scope.numbery
-        }
-      })
-      .then(function (data) {
-
-        $scope.details = data.status;
-        $scope.data = data;
-        $scope.tableR = true;
-        $scope.alertSuccess = true;
-        $scope.saludo = data;
-
-
-      }, function (response) {
-        //$scope.myWelcome = response.statusText;
-        //$scope.details = response;
-        $scope.tableR = false;
-        $scope.error = response.data;
-        $scope.alertM = true;
-
-      });
-
-
-  }
-  $scope.myTxt = "You have not yet clicked submit";
-  $scope.myFunc = function () {
-    $scope.myTxt = "You clicked submit!";
-  }
-
-
-});*/
