7e5f30265ea70be49c85bca2d6c837a84a54f67f
angelsilva94 <angelsilva_94@hotmail.com>
2017-03-10 10:24:17 -0600

    extra classes

---
+++ Shop/Shop.Models/DBModel/Address.cs
@@ -0,0 +1,31 @@
+﻿using Shop.Models.DBModel.Interfaces;
+using Shop.Models.DBModel.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Models.DBModel {
+    public class Address : IAddress {
+        public int AddressId { set; get; }
+        [Required]
+        public string street { set; get; }
+        [Required]
+        public string adress { set; get; }
+        [Required]
+        public string country { set; get; }
+        [Required]
+        public string city { set; get; }
+        [Required]
+        public string zip { set; get; }
+
+        [Required]
+        public int UserId { set; get; }
+
+        public virtual User User { set; get; }
+        
+        public virtual ICollection<Order> Orders { set; get; }
+    }
+}
---
+++ Shop/Shop.Models/DBModel/Interfaces/IUserType.cs
@@ -0,0 +1,9 @@
+﻿namespace Shop.Models.DBModel.Interfaces {
+    public interface IUserType {
+        string type { get; set; }
+        string typeDesc { get; set; }
+        User User { get; set; }
+        int UserId { get; set; }
+        
+    }
+}
