28d0e025471f14d796574f1a7f98ff592d37e53f
angelsilva94 <angelsilva_94@hotmail.com>
2017-03-27 13:24:47 -0600

    login/signup

---
+++ Shop/Shop/Scripts/route/route.js
@@ -0,0 +1,33 @@
+﻿/// <reference path="../angular.min.js" />
+
+var app = angular.module("shopModule", ["ngRoute"])
+                 .config(function ($routeProvider) {
+                     $routeProvider
+                        .when("/home", {
+                            templateUrl: "template/home.html",
+                            controller: "homeController"
+                        })
+                        .when("/login", {
+                            templateUrl: "template/login.html",
+                            controller: "loginController"
+                        })
+                        .when("/signup", {
+                            templateUrl: "template/signup.html",
+                            controller: "signupController"
+                        })
+
+
+
+                 })
+                 .controller("homeController", function ($scope) {
+
+                 })
+                 .controller("loginController", function ($scope) {
+
+                 })
+                 .controller("signupController", function ($scope) {
+
+                 })
+
+
+
---
+++ Shop/Shop/Scripts/scripts.js
@@ -0,0 +1,21 @@
+﻿// <reference path="angular.min.js" />
+
+//Create module
+var myApp = angular.module("myModule", []);
+
+// Register controller with the module
+myApp.controller("myController", function ($scope) {
+    var tec = [
+        { name: "c#", like: 0, dislike: 0 },
+        { name: "java", like: 0, dislike: 0 },
+        { name: "scala", like: 0, dislike: 0 },
+        { name: "c", like: 0, dislike: 0 },
+    ];
+    $scope.tec = tec;
+    $scope.inLikes = function (technology) {
+        technology.like++;
+    };
+    $scope.inDislikes = function (technology) {
+        technology.dislike++;
+    };
+});
