e31e757be865c0f09c85223715f929aa3d793c8f
Otman Licona <otmanlicona@hotmail.com>
2017-05-15 12:01:07 -0500

    ContainerExtensions

---
+++ Shop/Shop/Extensions/ContainerExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using Autofac;
+using Autofac.Integration.WebApi;
+
+namespace Shop.Extensions
+{
+    internal static class ContainerExtensions
+    {
+        public static IContainer FinalizeAndBuild(this ContainerBuilder containerBuilder)
+        {
+            containerBuilder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+            return containerBuilder.Build();
+        }
+    }
+}
--- Shop/Shop/Startup.cs
+++ Shop/Shop/Startup.cs
@@ -1,6 +1,11 @@
 ﻿using Microsoft.Owin;
 using Owin;
+using Shop.Ioc;
 using System.Web.Http;
+using Shop.Extensions;
+using Autofac;
+using Autofac.Integration.WebApi;
+
 
 [assembly: OwinStartup(typeof(Shop.Startup))]
 
@@ -10,11 +15,16 @@ namespace Shop {
 
         public void Configuration(IAppBuilder app) {
             // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
-
+            var containerBuilder = new ContainerFactory();
+            var container = containerBuilder.CreateBuilder().FinalizeAndBuild();
             var config = new HttpConfiguration();
             config.MapHttpAttributeRoutes();
-            app.UseWebApi(config);
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+            
             config.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            app.UseAutofacMiddleware(container);
+            app.UseAutofacWebApi(config);
+            app.UseWebApi(config);
         }
     }
 }
